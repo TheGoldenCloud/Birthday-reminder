9fc7ea6db18ba69673d377e19e7e86cf2c4a674e
GoldenCloudPc <jvladimir54@yahoo.com>
2023-02-11 21:51:10 +0100

    Added photos and styles

--- birthdayReminder/src/main.jsx
+++ birthdayReminder/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import BirthdayCard from './birthdayCard'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('nav')).render(
   <React.StrictMode>
-    <App />
+    <BirthdayCard />
   </React.StrictMode>,
 )
