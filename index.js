4da406c4eaa836317ffe24d1e5c3f87ab8438790
ZmajoramaLinux <jvladimir54@yahoo.com>
2023-12-13 00:24:22 +0100

    Server data endpoint added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const pool = require('./db.js');
+const env = require('dotenv').config();
+
+app.use(cors({
+  origin: "*", 
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}))
 
 app.get('/data', (req, res) => {
   res.send('hello world');
@@ -8,7 +16,7 @@ app.get('/data', (req, res) => {
 
 app.get('/persons', async(req, res) => {
   let data = await pool.query('select * from Person');
-  res.send(data);
+  res.send(data[0]);
 })
 
-app.listen(3000,()=>{ console.log("Server started on port 3000") })
+app.listen(process.env.PORT,()=>{ console.log(`Server started on port ${process.env.PORT}`) })
