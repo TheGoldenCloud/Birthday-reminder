0eced3a1e4387f1b35cba55b7c643f61bfe74f91
ZmajoramaLinux <jvladimir54@yahoo.com>
2023-12-13 00:23:49 +0100

    Database update

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
-import mysql2 from 'mysql2';
-
-import dotenv from 'dotenv';
-dotenv.config();
+const mysql2 = require('mysql2');
+const env = require('dotenv').config();
 
 const pool = mysql2.createPool({
     host: process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise();
